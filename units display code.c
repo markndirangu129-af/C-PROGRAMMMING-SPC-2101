90f76f2aabb344d5a2023d75608c2c7abc7c2cb4
markndirangu129-af <markndirangu129@gmail.com>
2025-09-19 13:14:13 +0300

    Add files via upload

---
+++ units display code.c
@@ -0,0 +1,18 @@
+//programme to display semester units
+/*
+NAME:Mark Kiruhi
+REG NO.PA106/G/28727/25
+DESCRIPTION:Semester units dislay
+*/
+#include<stdio.h>
+
+int main (){
+    printf("introduction to programming and problem solving \n");
+    printf("discrete mathematics \n");
+    printf("mathematics for science \n");
+    printf("fundamentals of ict \n");
+    printf("computer systems and organization \n");
+    printf("fundamentals of system analysis & design \n");
+    printf("communication skills \n");
+    return 0;
+}
